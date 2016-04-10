0df64f4198affb42b276151602e3098edf55fe7b
Matteo Bigoi <bigo@crisidev.org>
2016-04-11 00:28:51 +0200

    Add testing to command wrapper

---
+++ commands_test.go
@@ -0,0 +1,24 @@
+package main
+
+import "testing"
+
+func TestCmdOk(t *testing.T) {
+	err := Cmd("echo", "test")
+	if err != nil {
+		t.Fatalf("Expected err == nil, got %s", err)
+	}
+}
+
+func TestCmdErrorNonFound(t *testing.T) {
+	err := Cmd("anonexistentcommand")
+	if err == nil {
+		t.Fatalf("Expected err == nil, got %s", err)
+	}
+}
+
+func TestCmdBadReturnCode(t *testing.T) {
+	err := Cmd("cd", "/anonexistenpath")
+	if err == nil {
+		t.Fatalf("Expected err == nil, got %s", err)
+	}
+}
